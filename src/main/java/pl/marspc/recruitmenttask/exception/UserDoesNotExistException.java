95ea3c23c03705f2f87ac0ec1d81fab5156509e2
marlenaJakubowska <mar.jakubowska90@gmail.com>
2023-08-25 23:40:41 +0200

    Clean up

--- src/main/java/pl/marspc/recruitmenttask/exception/UserDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package pl.marspc.recruitmenttask.exception;
-
-public class UserDoesNotExistException extends RuntimeException {
-    public UserDoesNotExistException() {
-        super("User with the provided login does not exist");
-    }
-}
